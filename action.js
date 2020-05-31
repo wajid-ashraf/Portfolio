5793910e89ab9ee38a8bc642f78f23bb4077acbf
wajid <vajid.web@gmail.com>
2020-05-31 19:25:38 +0500

    active nav add final

--- action.js
+++ action.js
@@ -1,3 +1,18 @@
 $('.press').click(function () {
   $('.collapse').collapse('hide');
 });
+
+$('#navbarNavAltMarkup').click(function (e) {
+  $('.press').removeClass('active--nav');
+  let hasclass = $(e.target).hasClass('.active--nav');
+  if (hasclass == false) {
+    $(e.target).addClass('active--nav');
+  }
+});
+$('.list-group-item').click(function (e) {
+  $('.list-group-item').removeClass('active--nav');
+  let havclass = $(e.target).hasClass('.active--nav');
+  if (havclass == false) {
+    $(e.target).addClass('active--nav');
+  }
+});
